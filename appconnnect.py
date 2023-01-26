6b61d8a85deec0b9c52573dd785821bbf9672c54
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-26 23:10:45 +0530

    added shopdetails in database

--- appconnnect.py
+++ appconnnect.py
@@ -1,14 +1,18 @@
+
 from flask import *  
 import os 
 import qrcode
 import uuid
 from flask import Flask, render_template,request,flash,redirect,url_for,session
 import sqlite3
+import pandas as pd
 
 con=sqlite3.connect("database.db")
 cur=con.cursor()
-con.execute("create table if not exists shop(name text primary key,colordouble integer,colorsingle integer,blacksingle integer,blackdouvble integer)")
-cur.execute("select * from shop")
+#con.execute("create table if not exists shopdetails(name text primary key,colordouble integer,colorsingle integer,blacksingle integer,blackdouvble integer)")
+cur.execute("select * from shopdetails")
 data=cur.fetchall()
 print(data)
-con.close()
+
+
+
