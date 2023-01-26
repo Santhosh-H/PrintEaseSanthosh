3a853e5b49f31af7457ff0074974352b924b91d9
Santhosh <santhoshhariharan1393@gmail.com>
2023-01-26 14:28:24 +0530

    added login and sign up page
    the page will start with the login page and it will redirected to the main index html page if successsful

--- app.py
+++ app.py
@@ -2,12 +2,70 @@ from flask import *
 import os 
 import qrcode
 import uuid
+from flask import Flask, render_template,request,flash,redirect,url_for,session
+import sqlite3
 
+app = Flask(__name__)
+app.secret_key="123"
 qr_codes={}
 
+con=sqlite3.connect("database.db")
+con.execute("create table if not exists work(pid integer primary key,name text,password text,contact integer,mail text)")
+con.close()
+
+@app.route('/')
+def index1():
+    return render_template('index1.html')
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method=='POST':
+        name=request.form['name']
+        password=request.form['password']
+        con=sqlite3.connect("database.db")
+        con.row_factory=sqlite3.Row
+        cur=con.cursor()
+        cur.execute("select * from work where name=? and password=?",(name,password))
+        data=cur.fetchone()
+
+        if data:
+            session["name"]=data["name"]
+            session["password"]=data["password"]
+            return redirect("customer")
+        else:
+            flash("Username and Password Mismatch","danger")
+    return redirect(url_for("index"))
 
 
-app = Flask(__name__) 
+@app.route('/customer',methods=["GET","POST"])
+def customer():
+    return render_template("index.html")
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        try:
+            name=request.form['name']
+            password=request.form['password']
+            contact=request.form['contact']
+            mail=request.form['mail']
+            con=sqlite3.connect("database.db")
+            cur=con.cursor()
+            cur.execute("insert into work(name,password,contact,mail)values(?,?,?,?)",(name,password,contact,mail))
+            con.commit()
+            flash("Record Added  Successfully","success")
+        except:
+            flash("Error in Insert Operation","danger")
+        finally:
+            return redirect(url_for("index"))
+            con.close()
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
 
 @app.route('/')
--- py.py
+++
@@ -1,4 +0,0 @@
-import uuid
-
-unique_id = str(uuid.uuid4())
-print(unique_id)
